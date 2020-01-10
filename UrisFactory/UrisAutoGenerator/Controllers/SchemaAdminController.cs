f8e3bedba09243f7612bd333c1fa0db1fe69b7be
jruiz <jruiz@RIAMPPC10>
2020-01-10 09:44:31 +0100

    Add operation with schema config
    add operation download of config.json

---
+++ UrisFactory/UrisAutoGenerator/Controllers/SchemaAdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using UrisFactory.Models.Services;
+
+namespace UrisFactory.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class SchemaAdminController : Controller
+    {        
+        [HttpGet(Name="getSchema")]
+        public FileResult GetSchema()
+        {
+            string contentType = "";
+            contentType = SchemaConfigOperations.GetContentType();
+            return File(SchemaConfigOperations.GetFileData(), contentType);
+        }
+
+       //public IActionResult ReplaceSchemaConfig(File newSchemaConfig)
+       // {
+
+       // }
+    }
+}
---
+++ UrisFactory/UrisAutoGenerator/Models/Services/SchemaConfigOperations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrisFactory.Models.Services
+{
+    public static class SchemaConfigOperations
+    {
+        private static string configPath = "config/UrisConfig.json";
+
+        public static string GetContentType()
+        {
+            string contentType = "";
+            new FileExtensionContentTypeProvider().TryGetContentType(Path.GetFileName(configPath), out contentType);
+            return contentType;
+        }
+
+        public static byte[] GetFileData()
+        {
+            return File.ReadAllBytes(configPath);
+        }
+    }
+}
