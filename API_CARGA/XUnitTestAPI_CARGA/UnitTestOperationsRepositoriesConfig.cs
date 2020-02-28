aa5e500ebe8af2a5b7faedfdf52108ab011855b5
jruiz <jruiz@RIAMPPC10>
2020-02-28 08:20:10 +0100

    Add Test

---
+++ API_CARGA/XUnitTestAPI_CARGA/UnitTestOperationsRepositoriesConfig.cs
@@ -0,0 +1,54 @@
+using API_CARGA.Models.Entities;
+using API_CARGA.Models.Services;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace XUnitTestAPI_CARGA
+{
+    public class UnitTestOperationsRepositoriesConfig
+    {
+        [Fact]
+        public void GetConfigRepository()
+        {
+            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+            List<RepositoryConfig> listaRepositorios = repositoriesConfigMockService.GetRepositoryConfigs();
+            if (listaRepositorios.Count > 0)
+            {
+                RepositoryConfig repositoryConfig = listaRepositorios[0];
+                RepositoryConfig repositoryConfigGetByID = repositoriesConfigMockService.GetRepositoryConfigById(repositoryConfig.RepositoryConfigID);
+                Assert.True(repositoryConfig.Name.Equals(repositoryConfigGetByID.Name));
+            }
+            
+        }
+
+        [Fact]
+        public void DeleteConfigRepository()
+        {
+            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+            RepositoryConfig repositoryConfig = repositoriesConfigMockService.GetRepositoryConfigByName("ConfigRepository_1");
+            repositoriesConfigMockService.RemoveRepositoryConfig(repositoryConfig.RepositoryConfigID);
+            repositoryConfig = repositoriesConfigMockService.GetRepositoryConfigById(repositoryConfig.RepositoryConfigID);
+            Assert.Null(repositoryConfig);
+        }
+
+        [Fact]
+        public void AddConfigRepository()
+        {
+            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+            RepositoryConfig repositoryConfigToAdd = new RepositoryConfig
+            {
+                RepositoryConfigID = Guid.NewGuid(),
+                Name = "Un repositorio para configurarlos a todos",
+                Url = "config\\repository",
+                OauthToken = "12weq1"
+            };
+            Guid identifierAdded = repositoriesConfigMockService.AddRepositoryConfig(repositoryConfigToAdd);
+            RepositoryConfig repositoryConfig = repositoriesConfigMockService.GetRepositoryConfigById(identifierAdded);
+            Assert.True(repositoryConfigToAdd.Name.Equals(repositoryConfig.Name));
+        }
+
+
+
+    }
+}
