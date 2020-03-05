241d13e9bf634a1c82cb654d1685a5a47f582e7b
jruiz <jruiz@RIAMPPC10>
2020-03-05 14:49:19 +0100

    Update ShapeConfigController.cs

--- Api_Carga_Client/ApiCargaWebInterface/ApiCargaWebInterface/Controllers/ShapeConfigController.cs
+++ Api_Carga_Client/ApiCargaWebInterface/ApiCargaWebInterface/Controllers/ShapeConfigController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ApiCargaWebInterface.Extra.Exceptions;
 using ApiCargaWebInterface.Models.Services;
 using ApiCargaWebInterface.ViewModels;
