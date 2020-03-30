a10d05af21040f7dab9e121672398ce228cf1c2a
jruiz <jruiz@RIAMPPC10>
2020-03-30 16:08:51 +0200

    Update InfoUriStructure.cs

--- UrisFactory/UrisAutoGenerator/ViewModels/InfoUriStructure.cs
+++ UrisFactory/UrisAutoGenerator/ViewModels/InfoUriStructure.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UrisFactory.Models.ConfigEntities;
+using UrisFactory.Models.ConfigEntities; 
 
 namespace UrisFactory.ViewModels
 {
