071e18bd316eb0576b15a121f2732b730e1a97f6
jruiz <jruiz@RIAMPPC10>
2020-05-15 12:42:39 +0200

    Authorize Dashboard hangfire

---
+++ CronConfigure/CronConfigure/Filters/HangfireDashboardNoAuthorizationFilter.cs
@@ -0,0 +1,16 @@
+﻿using Hangfire.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CronConfigure.Filters
+{
+    public class HangfireDashboardNoAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext dashboardContext)
+        {
+            return true;
+        }
+    }
+}
