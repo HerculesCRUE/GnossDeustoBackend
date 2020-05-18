55c0e7ae585645bd5d515aadc6e8ef6b15993f4a
alvaropalacios <alvaropalacios@gnoss.com>
2020-05-18 17:14:44 +0200

    Update UnitTestOperationsValidation.cs

--- API_CARGA/XUnitTestAPI_CARGA/UnitTestOperationsValidation.cs
+++ API_CARGA/XUnitTestAPI_CARGA/UnitTestOperationsValidation.cs
@@ -1,7 +1,12 @@
-﻿using API_CARGA.Models.Entities;
+﻿using API_CARGA.Controllers;
+using API_CARGA.Models.Entities;
 using API_CARGA.Models.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using Xunit;
 
 namespace XUnitTestAPI_CARGA
@@ -12,7 +17,8 @@ namespace XUnitTestAPI_CARGA
         public void GetShape()
         {
             ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            List<ShapeConfig> listaRepositorios = shapesConfigMockService.GetShapesConfigs();
+            ValidationController validationController = new ValidationController(shapesConfigMockService);            
+            List<ShapeConfig> listaRepositorios = (List<ShapeConfig>)(((OkObjectResult)validationController.GetShape()).Value);
             Assert.True(listaRepositorios.Count > 0);
         }
 
@@ -20,11 +26,12 @@ namespace XUnitTestAPI_CARGA
         public void GetShapeByID()
         {
             ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            List<ShapeConfig> listaRepositorios = shapesConfigMockService.GetShapesConfigs();
+            ValidationController validationController = new ValidationController(shapesConfigMockService);
+            List<ShapeConfig> listaRepositorios = (List<ShapeConfig>)(((OkObjectResult)validationController.GetShape()).Value);           
             if (listaRepositorios.Count > 0)
             {
                 ShapeConfig shapeConfig = listaRepositorios[0];
-                ShapeConfig shapeConfigGetByID = shapesConfigMockService.GetShapeConfigById(shapeConfig.ShapeConfigID);
+                ShapeConfig shapeConfigGetByID = (ShapeConfig)(((OkObjectResult)validationController.GetShape(shapeConfig.ShapeConfigID)).Value);
                 Assert.True(shapeConfig.Name.Equals(shapeConfigGetByID.Name));
             }
         }
@@ -33,26 +40,48 @@ namespace XUnitTestAPI_CARGA
         [Fact]
         public void AddConfigShape()
         {
-            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            ShapeConfig shapeConfigToAdd = new ShapeConfig()
-            {
-                ShapeConfigID = Guid.NewGuid(),
-                Name = "Circular Shape",
-                RepositoryID = Guid.NewGuid(),
-                Shape = "Circle"
-            };
-            Guid identifierAdded = shapesConfigMockService.AddShapeConfig(shapeConfigToAdd);
-            ShapeConfig shapeConfig = shapesConfigMockService.GetShapeConfigById(identifierAdded);
-            Assert.True(shapeConfigToAdd.Name.Equals(shapeConfig.Name));
+            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();            
+            ValidationController validationController = new ValidationController(shapesConfigMockService);
+
+            StringBuilder personShape = new StringBuilder();
+            personShape.AppendLine("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.");
+            personShape.AppendLine("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.");
+            personShape.AppendLine("@prefix xsd: <http://www.w3.org/2001/XMLSchema#>.");
+            personShape.AppendLine("@prefix xml: <http://www.w3.org/XML/1998/namespace>.");
+            personShape.AppendLine("@prefix ns: <http://www.w3.org/2003/06/sw-vocab-status/ns#>.");
+            personShape.AppendLine("@prefix sh: <http://www.w3.org/ns/shacl#>.");
+            personShape.AppendLine("@prefix roh: <http://purl.org/roh#>.");
+            personShape.AppendLine("@prefix foaf: <http://purl.org/roh/mirror/foaf#>.");
+            personShape.AppendLine("roh:foaf_PersonShape");
+            personShape.AppendLine("	a sh:NodeShape ;");
+            personShape.AppendLine("	sh:targetClass foaf:Person ;");
+            personShape.AppendLine("	sh:property roh:someValuesDataType__foaf__Person__foaf__firstName.");
+            personShape.AppendLine("roh:someValuesDataType__foaf__Person__foaf__firstName ");
+            personShape.AppendLine("	sh:severity sh:Violation;");
+            personShape.AppendLine("	sh:path foaf:firstName;");
+            personShape.AppendLine("	sh:qualifiedMinCount  1;");
+            personShape.AppendLine("	sh:qualifiedValueShape [");
+            personShape.AppendLine("		sh:datatype xsd:string;");
+            personShape.AppendLine("	].");
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(personShape.ToString());
+            writer.Flush();
+            stream.Position = 0;
+            var file = new FormFile(stream, 0, stream.Length, null, "rdf.xml");        
+            Guid identifierAdded = (Guid)(((OkObjectResult)validationController.AddShape("Shape Name", Guid.NewGuid(), file)).Value);
+            ShapeConfig shapeConfig = (ShapeConfig)(((OkObjectResult)validationController.GetShape(identifierAdded)).Value);
+            Assert.True(shapeConfig.Name.Equals("Shape Name"));
         }
 
         [Fact]
         public void DeleteConfigShape()
         {
             ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            ShapeConfig shapeConfig = shapesConfigMockService.GetShapesConfigs()[0];
-            shapesConfigMockService.RemoveShapeConfig(shapeConfig.ShapeConfigID);
-            shapeConfig = shapesConfigMockService.GetShapeConfigById(shapeConfig.ShapeConfigID);
+            ValidationController validationController = new ValidationController(shapesConfigMockService);
+            ShapeConfig shapeConfig =( (List<ShapeConfig>)(((OkObjectResult)validationController.GetShape()).Value))[0];
+            validationController.DeleteShape(shapeConfig.ShapeConfigID);
+            shapeConfig = (ShapeConfig)(((OkObjectResult)validationController.GetShape(shapeConfig.ShapeConfigID)).Value);
             Assert.Null(shapeConfig);
         }
 
@@ -60,13 +89,39 @@ namespace XUnitTestAPI_CARGA
         public void ModifyConfigShape()
         {
             ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            ShapeConfig shapeConfig = shapesConfigMockService.GetShapesConfigs()[0];
-            Random random = new Random();
-            string newName = "updatedShape_" + random.NextDouble();
-            shapeConfig.Name = newName;
-            shapesConfigMockService.ModifyShapeConfig(shapeConfig);
-            ShapeConfig updatedshapeConfig = shapesConfigMockService.GetShapeConfigById(shapeConfig.ShapeConfigID);
-            Assert.True(updatedshapeConfig.Name.Equals(newName));
+            ValidationController validationController = new ValidationController(shapesConfigMockService);
+            ShapeConfig shapeConfig = ((List<ShapeConfig>)(((OkObjectResult)validationController.GetShape()).Value))[0];
+
+            StringBuilder personShape = new StringBuilder();
+            personShape.AppendLine("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.");
+            personShape.AppendLine("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.");
+            personShape.AppendLine("@prefix xsd: <http://www.w3.org/2001/XMLSchema#>.");
+            personShape.AppendLine("@prefix xml: <http://www.w3.org/XML/1998/namespace>.");
+            personShape.AppendLine("@prefix ns: <http://www.w3.org/2003/06/sw-vocab-status/ns#>.");
+            personShape.AppendLine("@prefix sh: <http://www.w3.org/ns/shacl#>.");
+            personShape.AppendLine("@prefix roh: <http://purl.org/roh#>.");
+            personShape.AppendLine("@prefix foaf: <http://purl.org/roh/mirror/foaf#>.");
+            personShape.AppendLine("roh:foaf_PersonShape");
+            personShape.AppendLine("	a sh:NodeShape ;");
+            personShape.AppendLine("	sh:targetClass foaf:Person ;");
+            personShape.AppendLine("	sh:property roh:someValuesDataType__foaf__Person__foaf__firstName.");
+            personShape.AppendLine("roh:someValuesDataType__foaf__Person__foaf__firstName ");
+            personShape.AppendLine("	sh:severity sh:Violation;");
+            personShape.AppendLine("	sh:path foaf:firstName;");
+            personShape.AppendLine("	sh:qualifiedMinCount  1;");
+            personShape.AppendLine("	sh:qualifiedValueShape [");
+            personShape.AppendLine("		sh:datatype xsd:string;");
+            personShape.AppendLine("	].");
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(personShape.ToString());
+            writer.Flush();
+            stream.Position = 0;
+            var file = new FormFile(stream, 0, stream.Length, null, "rdf.xml");
+
+            validationController.ModifyShape(shapeConfig.ShapeConfigID, shapeConfig.Name, shapeConfig.RepositoryID, file);
+            ShapeConfig updatedshapeConfig = (ShapeConfig)(((OkObjectResult)validationController.GetShape(shapeConfig.ShapeConfigID)).Value);
+            Assert.True(updatedshapeConfig.Shape.Equals(personShape.ToString()));
         }
 
     }
