6c2c63648fe2298a32618414857e8612cde579e5
jruiz <jruiz@RIAMPPC10>
2020-06-02 16:09:47 +0200

    Add IdentityServer Project

---
+++ IdentityServerHecules/IdentityServerHecules/Config.cs
@@ -0,0 +1,101 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerHecules
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = {"api1"},
+                AccessTokenLifetime = 86400
+                //AccessTokenType = AccessTokenType.Reference
+            },
+            new Client
+            {
+                ClientId = "carga",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = {"apiCarga"},
+                AccessTokenLifetime = 86400
+                //AccessTokenType = AccessTokenType.Reference
+            },
+            new Client
+            {
+                ClientId = "cron",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secretCron".Sha256())
+                },
+                AllowedScopes = {"apiCron"},
+                AccessTokenLifetime = 86400
+                //AccessTokenType = AccessTokenType.Reference
+            }
+            ,
+            new Client
+            {
+                ClientId = "Web",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("master".Sha256())
+                },
+                AllowedScopes = {"apiCron", "apiCarga"},
+                AccessTokenLifetime = 86400
+                //AccessTokenType = AccessTokenType.Reference
+            },
+            new Client
+            {
+                ClientId = "urisFactory",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secretUris".Sha256())
+                },
+                AllowedScopes = { "apiUrisFactory"},
+                AccessTokenLifetime = 86400
+                //AccessTokenType = AccessTokenType.Reference
+            }
+            ,
+            new Client
+            {
+                ClientId = "OAIPMH",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secretOAIPMH".Sha256())
+                },
+                AllowedScopes = { "apiOAIPMH"},
+                AccessTokenLifetime = 86400
+                //AccessTokenType = AccessTokenType.Reference
+            }
+        };
+        }
+
+        // APIs allowed to access the Auth server
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+        {
+            new ApiResource("api1", "My Api"),new ApiResource("apiCarga", "My ApiCarga"),new ApiResource("apiCron", "My ApiCron"),new ApiResource("apiUrisFactory", "My ApiUrisFactory"),new ApiResource("apiOAIPMH", "My apiOAIPMH")
+        };
+        }
+    }
+}
---
+++ IdentityServerHecules/IdentityServerHecules/Persist/PersistedGrantStore.cs
@@ -0,0 +1,83 @@
+﻿using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerHecules.Persist
+{
+    public class PersistedGrantStore : IPersistedGrantStore
+    {
+        private readonly IPersistedGrantServiceP persistedGrantService;
+
+        public PersistedGrantStore(IPersistedGrantServiceP persistedGrantService)
+        {
+            this.persistedGrantService = persistedGrantService;
+        }
+        public Task<IEnumerable<PersistedGrant>> GetAllAsync(string subjectId)
+        {
+            var persistedGrants = this.persistedGrantService.GetAll(subjectId).ToList();
+
+            var model = persistedGrants.Select(x => x.ToModel());
+
+            return Task.FromResult(model);
+        }
+
+        public Task<PersistedGrant> GetAsync(string key)
+        {
+            var persistedGrant = this.persistedGrantService.Get(key);
+            var model = persistedGrant?.ToModel();
+
+            return Task.FromResult(model);
+        }
+
+        public Task RemoveAllAsync(string subjectId, string clientId)
+        {
+            var persistedGrants = this.persistedGrantService.GetAll(subjectId, clientId);
+
+            this.persistedGrantService.RemoveAll(persistedGrants);
+            
+            return Task.FromResult(0);
+        }
+
+        public Task RemoveAllAsync(string subjectId, string clientId, string type)
+        {
+            var persistedGrants = this.persistedGrantService.GetAll(subjectId, clientId, type);
+
+            this.persistedGrantService.RemoveAll(persistedGrants);
+
+
+            return Task.FromResult(0);
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            var persistedGrant = this.persistedGrantService.Get(key);
+
+            if (persistedGrant != null)
+            {
+                this.persistedGrantService.Remove(persistedGrant);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public Task StoreAsync(PersistedGrant grant)
+        {
+            var existing = this.persistedGrantService.Get(grant.Key);
+            if(existing == null)
+            {
+                var persistedGrant = grant.ToEntity();
+                this.persistedGrantService.Add(persistedGrant);
+            }
+            else
+            {
+                this.persistedGrantService.Update(existing);
+            }
+            return Task.FromResult(0);
+        }
+    }
+}
