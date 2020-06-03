4c43870602dd08e1f2cb96f0dff3fc63842b0b12
jruiz <jruiz@RIAMPPC10>
2020-06-03 11:12:31 +0200

    Download Shape file

--- FrontEndCarga/ApiCargaWebInterface/Controllers/ShapeConfigController.cs
+++ FrontEndCarga/ApiCargaWebInterface/Controllers/ShapeConfigController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using ApiCargaWebInterface.Extra.Exceptions;
 using ApiCargaWebInterface.Models.Services;
 using ApiCargaWebInterface.ViewModels;
@@ -31,6 +32,23 @@ namespace ApiCargaWebInterface.Controllers
             if (result != null)
             {
                 return View(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("[Controller]/download/{id}")]
+        public IActionResult DownloadShape(Guid id)
+        {
+            ShapeConfigViewModel result = _serviceApi.GetShapeConfig(id);
+            if (result != null)
+            {
+                var content = new System.IO.MemoryStream(Encoding.ASCII.GetBytes(result.Shape));
+                var contentType = "APPLICATION/octet-stream";
+                var fileName = $"{id}_shape.txt";
+                return File(content, contentType, fileName);
             }
             else
             {
