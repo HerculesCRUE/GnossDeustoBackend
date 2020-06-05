c7afbfa21a4a6d706a33821821e22ce7c99601f4
jruiz <jruiz@RIAMPPC10>
2020-06-05 15:22:42 +0200

    Add comments

--- UrisFactory/UrisAutoGenerator/Models/Services/ISchemaConfigOperations.cs
+++ UrisFactory/UrisAutoGenerator/Models/Services/ISchemaConfigOperations.cs
@@ -6,11 +6,27 @@ using System.Threading.Tasks;
 
 namespace UrisFactory.Models.Services
 {
+    ///<summary>
+    ///Interfaz para las operaciones con el fichero de configuración
+    ///</summary>
     public interface ISchemaConfigOperations
     {
+        ///<summary>
+        ///Obtiene el content type del fichero
+        ///</summary>
         public abstract string GetContentType();
+        ///<summary>
+        ///Obtiene los bytes con el fichero de configuracion
+        ///</summary>
         public abstract byte[] GetFileSchemaData();
+        ///<summary>
+        ///Guarda el fichero de configuracion
+        ///</summary>
+        ///<param name="formFile">cadena de texto emulando el json del fichero de configuracion</param>
         public abstract bool SaveConfigFile(IFormFile formFile);
+        ///<summary>
+        ///Guarda el fichero de configuracion
+        ///</summary>
         public abstract bool SaveConfigJson();
     }
 }
