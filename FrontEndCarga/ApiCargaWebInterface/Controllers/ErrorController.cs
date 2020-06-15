2f1bb1532c7aa1821247a9ec71e6093f5fd09910
jruiz <jruiz@RIAMPPC10>
2020-06-15 10:38:56 +0200

    Copyright

--- FrontEndCarga/ApiCargaWebInterface/Controllers/ErrorController.cs
+++ FrontEndCarga/ApiCargaWebInterface/Controllers/ErrorController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿// Copyright (c) UTE GNOSS - UNIVERSIDAD DE DEUSTO
+// Licenciado bajo la licencia GPL 3. Ver https://www.gnu.org/licenses/gpl-3.0.html
+// Proyecto Hércules ASIO Backend SGI. Ver https://www.um.es/web/hercules/proyectos/asio
+// Redireccionamiento a páginas de errores
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiCargaWebInterface.Controllers
--- FrontEndCarga/ApiCargaWebInterface/Models/Services/CallRepositoryConfigApiService.cs
+++ FrontEndCarga/ApiCargaWebInterface/Models/Services/CallRepositoryConfigApiService.cs
@@ -1,4 +1,8 @@
-﻿using ApiCargaWebInterface.ViewModels;
+﻿// Copyright (c) UTE GNOSS - UNIVERSIDAD DE DEUSTO
+// Licenciado bajo la licencia GPL 3. Ver https://www.gnu.org/licenses/gpl-3.0.html
+// Proyecto Hércules ASIO Backend SGI. Ver https://www.um.es/web/hercules/proyectos/asio
+// Servicio para hacer llamadas al api de repositorios
+using ApiCargaWebInterface.ViewModels;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
