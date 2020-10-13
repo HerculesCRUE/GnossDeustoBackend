0dccd66f2cc3edeae9fca8d9491e65489bb8c0ef
jruiz <jruiz@RIAMPPC10>
2020-10-13 16:32:47 +0200

    Documentation

--- FrontEndCarga/ApiCargaWebInterface/Models/Services/ReplaceUsesService.cs
+++ FrontEndCarga/ApiCargaWebInterface/Models/Services/ReplaceUsesService.cs
@@ -1,19 +1,18 @@
-﻿using ApiCargaWebInterface.Models.Entities;
+﻿// Copyright (c) UTE GNOSS - UNIVERSIDAD DE DEUSTO
+// Licenciado bajo la licencia GPL 3. Ver https://www.gnu.org/licenses/gpl-3.0.html
+// Proyecto Hércules ASIO Backend SGI. Ver https://www.um.es/web/hercules/proyectos/asio
+// Clase para cargar en el modelo los resultados de las directivas
+using ApiCargaWebInterface.Models.Entities;
 using ApiCargaWebInterface.Utility;
 using ApiCargaWebInterface.ViewModels;
-using CsvHelper;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ApiCargaWebInterface.Models.Services
 {
+    /// <summary>
+    /// Clase para cargar en el modelo los resultados de las directivas
+    /// </summary>
     public class ReplaceUsesService
     {
         public ICallService _callService;
@@ -27,6 +26,12 @@ namespace ApiCargaWebInterface.Models.Services
             _configUrlCronService = configUrlCronService;
             _callTokenService = callTokenService;
         }
+        /// <summary>
+        /// Método que carga en el modelo los resultados obtenidos al llamar a los servicios especificados en las directivas
+        /// </summary>
+        /// <param name="htmlContent">html de la página con las directivas</param>
+        /// <param name="dataModel">modelo donde cargar los datos</param>
+        /// <returns>Modelo con los datos cargados</returns>
         public CmsDataViewModel PageWithDirectives(string htmlContent, CmsDataViewModel dataModel)
         {
             dataModel.Results = new List<string>();
@@ -45,6 +50,11 @@ namespace ApiCargaWebInterface.Models.Services
             return dataModel;
         }
 
+        /// <summary>
+        /// Método que devuelve un diccionario con todos los tipos de directiva y posición de su aparición
+        /// </summary>
+        /// <param name="htmlContent">Contenido de la página</param>
+        /// <returns>Diccionario con clave su posición de aparición y valor el tipo de directiva</returns>
         private Dictionary<int, string> Directives(string htmlContent)
         {
             Dictionary<int, string> directives = new Dictionary<int, string>();
@@ -73,7 +83,12 @@ namespace ApiCargaWebInterface.Models.Services
             }  
             return directives;
         }
-
+        /// <summary>
+        /// Obtiene el resultado en formato json de la llamada al api
+        /// </summary>
+        /// <param name="htmlContent">contenido html</param>
+        /// <param name="ocurrence">Posición de la cual hay que mirar</param>
+        /// <returns>resultado de la llamada en formato json</returns>
         private string Api(string htmlContent, int ocurrence)
         {
             int first = htmlContent.IndexOf(DirectivesList.Api, ocurrence);
@@ -96,7 +111,12 @@ namespace ApiCargaWebInterface.Models.Services
             string result = _callService.CallGetApi(url, "", token);
             return result;
         }
-
+        /// <summary>
+        /// obtiene el formato en csv de la consulta sparql obtenida de la directiva
+        /// </summary>
+        /// <param name="htmlContent">contenido html</param>
+        /// <param name="ocurrence">Posición de la cual hay que mirar</param>
+        /// <returns>Resultado de la llamada en formato csv</returns>
         private string Sparql(string htmlContent, int ocurrence)
         {
             int first = htmlContent.IndexOf(DirectivesList.Sparql, ocurrence);
@@ -109,10 +129,5 @@ namespace ApiCargaWebInterface.Models.Services
             return result;
         }
 
-    }
-    public class PruebaSparql
-    {
-        public string type { get; set; }
-        public int count { get; set; }
     }
 }
--- FrontEndCarga/ApiCargaWebInterface/Utility/DirectivesList.cs
+++ FrontEndCarga/ApiCargaWebInterface/Utility/DirectivesList.cs
@@ -1,31 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// Copyright (c) UTE GNOSS - UNIVERSIDAD DE DEUSTO
+// Licenciado bajo la licencia GPL 3. Ver https://www.gnu.org/licenses/gpl-3.0.html
+// Proyecto Hércules ASIO Backend SGI. Ver https://www.um.es/web/hercules/proyectos/asio
+// Clase con el listado de directivas
 
 namespace ApiCargaWebInterface.Utility
 {
+    /// <summary>
+    /// Clase con el listado de directivas
+    /// </summary>
     public class DirectivesList
     {
         private static string sparql = "@*<% sparql";
         private static string api = "@*<% api";
         private static string directive = "@*<%";
         private static string endDirective = "/%>*@";
-
+        /// <summary>
+        /// Obtiene el inicio de la directiva api
+        /// </summary>
         public static string Api
         {
             get { return api; }
         }
-
+        /// <summary>
+        /// Obtiene el inicio de la directiva sparql
+        /// </summary>
         public static string Sparql
         {
             get { return sparql; }
         }
-
+        /// <summary>
+        /// Obtiene la etiqueta de inicio de una directiva
+        /// </summary>
         public static string Directive
         {
             get { return directive; }
         }
+        /// <summary>
+        /// Obtiene la etiqueta de fin de una directiva
+        /// </summary>
         public static string EndDirective
         {
             get { return endDirective; }
