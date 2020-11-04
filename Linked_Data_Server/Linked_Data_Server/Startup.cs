a71d4f7eb70da58119b94be034054bf3d001fa6a
alvaropalacios <alvaropalacios@gnoss.com>
2020-11-04 12:13:17 +0100

    Update Startup.cs

--- Linked_Data_Server/Linked_Data_Server/Startup.cs
+++ Linked_Data_Server/Linked_Data_Server/Startup.cs
@@ -45,7 +45,7 @@ namespace Linked_Data_Server
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
