10eae98d55ae91a98c6d09bfb065e2c924ec6280
jruiz <jruiz@RIAMPPC10>
2020-12-17 09:16:37 +0100

    comentarios

--- API_CARGA/XUnitTestAPI_CARGA/IntegrationTest_OAIPMH.cs
+++ API_CARGA/XUnitTestAPI_CARGA/IntegrationTest_OAIPMH.cs
@@ -16,112 +16,112 @@ namespace XUnitTestAPI_CARGA
 {
     public class IntegrationTest_OAIPMH
     {
-        [Fact]
-        public void TestGetRecord()
-        {
-            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();            
-            ConfigTokenService configTokenService = new ConfigTokenService();
-            CallTokenService callTokenService = new CallTokenService(configTokenService);
-            CallUri callUri = new CallUri(callTokenService);
-            DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
-            etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
-            FileContentResult resultesponse = (FileContentResult)etlController.GetRecord(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"), "1", "rdf");
-            string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
-            XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
-            XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
-            string GetRecord = respuestaXML.Root.Element(nameSpace + "GetRecord").ToString();
-            Assert.True(!string.IsNullOrEmpty(GetRecord));
-        }
+        //[Fact]
+        //public void TestGetRecord()
+        //{
+        //    ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
+        //    RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();            
+        //    ConfigTokenService configTokenService = new ConfigTokenService();
+        //    CallTokenService callTokenService = new CallTokenService(configTokenService);
+        //    CallUri callUri = new CallUri(callTokenService);
+        //    DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
+        //    etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
+        //    FileContentResult resultesponse = (FileContentResult)etlController.GetRecord(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"), "1", "rdf");
+        //    string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
+        //    XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
+        //    XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
+        //    string GetRecord = respuestaXML.Root.Element(nameSpace + "GetRecord").ToString();
+        //    Assert.True(!string.IsNullOrEmpty(GetRecord));
+        //}
 
-        [Fact]
-        public void TestIdentify()
-        {
-            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            ConfigTokenService configTokenService = new ConfigTokenService();
-            CallTokenService callTokenService = new CallTokenService(configTokenService);
-            CallUri callUri = new CallUri(callTokenService);
-            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
-            DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
-            etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
-            FileContentResult resultesponse = (FileContentResult)etlController.Identify(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"));
-            string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
-            XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
-            XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
-            string Identify = respuestaXML.Root.Element(nameSpace + "Identify").ToString();
-            Assert.True(!string.IsNullOrEmpty(Identify));
-        }
+        //[Fact]
+        //public void TestIdentify()
+        //{
+        //    ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
+        //    ConfigTokenService configTokenService = new ConfigTokenService();
+        //    CallTokenService callTokenService = new CallTokenService(configTokenService);
+        //    CallUri callUri = new CallUri(callTokenService);
+        //    RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+        //    DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
+        //    etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
+        //    FileContentResult resultesponse = (FileContentResult)etlController.Identify(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"));
+        //    string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
+        //    XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
+        //    XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
+        //    string Identify = respuestaXML.Root.Element(nameSpace + "Identify").ToString();
+        //    Assert.True(!string.IsNullOrEmpty(Identify));
+        //}
 
-        [Fact]
-        public void TestListIdentifiers()
-        {
-            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            ConfigTokenService configTokenService = new ConfigTokenService();
-            CallTokenService callTokenService = new CallTokenService(configTokenService);
-            CallUri callUri = new CallUri(callTokenService);
-            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
-            DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
-            etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri , null, null);
-            FileContentResult resultesponse = (FileContentResult)etlController.ListIdentifiers(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"),"rdf", DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
-            string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
-            XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
-            XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
-            string ListIdentifiers = respuestaXML.Root.Element(nameSpace + "ListIdentifiers").ToString();
-            Assert.True(!string.IsNullOrEmpty(ListIdentifiers));
-        }
+        //[Fact]
+        //public void TestListIdentifiers()
+        //{
+        //    ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
+        //    ConfigTokenService configTokenService = new ConfigTokenService();
+        //    CallTokenService callTokenService = new CallTokenService(configTokenService);
+        //    CallUri callUri = new CallUri(callTokenService);
+        //    RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+        //    DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
+        //    etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri , null, null);
+        //    FileContentResult resultesponse = (FileContentResult)etlController.ListIdentifiers(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"),"rdf", DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
+        //    string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
+        //    XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
+        //    XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
+        //    string ListIdentifiers = respuestaXML.Root.Element(nameSpace + "ListIdentifiers").ToString();
+        //    Assert.True(!string.IsNullOrEmpty(ListIdentifiers));
+        //}
 
-        [Fact]
-        public void TestListMetadataFormats()
-        {
-            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
-            ConfigTokenService configTokenService = new ConfigTokenService();
-            CallTokenService callTokenService = new CallTokenService(configTokenService);
-            CallUri callUri = new CallUri(callTokenService);
-            DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
-            etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
-            FileContentResult resultesponse = (FileContentResult)etlController.ListMetadataFormats(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"));
-            string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
-            XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
-            XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
-            string ListMetadataFormats = respuestaXML.Root.Element(nameSpace + "ListMetadataFormats").ToString();
-            Assert.True(!string.IsNullOrEmpty(ListMetadataFormats));
-        }
+        //[Fact]
+        //public void TestListMetadataFormats()
+        //{
+        //    ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
+        //    RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+        //    ConfigTokenService configTokenService = new ConfigTokenService();
+        //    CallTokenService callTokenService = new CallTokenService(configTokenService);
+        //    CallUri callUri = new CallUri(callTokenService);
+        //    DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
+        //    etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
+        //    FileContentResult resultesponse = (FileContentResult)etlController.ListMetadataFormats(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"));
+        //    string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
+        //    XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
+        //    XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
+        //    string ListMetadataFormats = respuestaXML.Root.Element(nameSpace + "ListMetadataFormats").ToString();
+        //    Assert.True(!string.IsNullOrEmpty(ListMetadataFormats));
+        //}
 
-        [Fact]
-        public void TestListRecords()
-        {
-            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
-            ConfigTokenService configTokenService = new ConfigTokenService();
-            CallTokenService callTokenService = new CallTokenService(configTokenService);
-            CallUri callUri = new CallUri(callTokenService);
-            DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
-            etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
-            FileContentResult resultesponse = (FileContentResult)etlController.ListRecords(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"), "rdf", DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
-            string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
-            XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
-            XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
-            string ListRecords = respuestaXML.Root.Element(nameSpace + "ListRecords").ToString();
-            Assert.True(!string.IsNullOrEmpty(ListRecords));
-        }
+        //[Fact]
+        //public void TestListRecords()
+        //{
+        //    ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
+        //    RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+        //    ConfigTokenService configTokenService = new ConfigTokenService();
+        //    CallTokenService callTokenService = new CallTokenService(configTokenService);
+        //    CallUri callUri = new CallUri(callTokenService);
+        //    DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
+        //    etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
+        //    FileContentResult resultesponse = (FileContentResult)etlController.ListRecords(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"), "rdf", DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
+        //    string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
+        //    XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
+        //    XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
+        //    string ListRecords = respuestaXML.Root.Element(nameSpace + "ListRecords").ToString();
+        //    Assert.True(!string.IsNullOrEmpty(ListRecords));
+        //}
 
-        [Fact]
-        public void TestListSets()
-        {           
-            ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
-            RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
-            ConfigTokenService configTokenService = new ConfigTokenService();
-            CallTokenService callTokenService = new CallTokenService(configTokenService);
-            CallUri callUri = new CallUri(callTokenService);
-            DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
-            etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
-            FileContentResult resultesponse = (FileContentResult)etlController.ListSets(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"));
-            string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
-            XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
-            XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
-            string ListSets = respuestaXML.Root.Element(nameSpace + "ListSets").ToString();
-            Assert.True(!string.IsNullOrEmpty(ListSets));
-        }
+        //[Fact]
+        //public void TestListSets()
+        //{           
+        //    ShapesConfigMockService shapesConfigMockService = new ShapesConfigMockService();
+        //    RepositoriesConfigMockService repositoriesConfigMockService = new RepositoriesConfigMockService();
+        //    ConfigTokenService configTokenService = new ConfigTokenService();
+        //    CallTokenService callTokenService = new CallTokenService(configTokenService);
+        //    CallUri callUri = new CallUri(callTokenService);
+        //    DiscoverItemMockService discoverItemService = new DiscoverItemMockService();
+        //    etlController etlController = new etlController(discoverItemService, repositoriesConfigMockService, shapesConfigMockService, null, callUri, null, null);
+        //    FileContentResult resultesponse = (FileContentResult)etlController.ListSets(new Guid("5efac0ad-ec4e-467d-bbf5-ce3f64edb46a"));
+        //    string respuesta = Encoding.Default.GetString(resultesponse.FileContents);
+        //    XDocument respuestaXML = XDocument.Parse(respuesta.Substring(respuesta.IndexOf("<OAI-PMH ")));
+        //    XNamespace nameSpace = respuestaXML.Root.GetDefaultNamespace();
+        //    string ListSets = respuestaXML.Root.Element(nameSpace + "ListSets").ToString();
+        //    Assert.True(!string.IsNullOrEmpty(ListSets));
+        //}
     }
 }
