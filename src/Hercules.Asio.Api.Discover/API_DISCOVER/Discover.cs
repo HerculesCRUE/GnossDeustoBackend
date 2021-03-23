033252a1cc813c662820177929c2240555b7a194
alvaropalacios-gnoss <alvaro@riam.local>
2021-03-23 08:29:22 +0100

    Update Discover.cs

--- src/Hercules.Asio.Api.Discover/API_DISCOVER/Discover.cs
+++ src/Hercules.Asio.Api.Discover/API_DISCOVER/Discover.cs
@@ -38,8 +38,6 @@ namespace API_DISCOVER
         private readonly IServiceScopeFactory _serviceScopeFactory;
 
         private readonly static string mPropertySGIRohCrisIdentifier = "http://purl.org/roh#crisIdentifier";
-        private DiscoverItemBDService _discoverItemBDService { get; }
-        private ProcessDiscoverStateJobBDService _processDiscoverStateJobBDService { get; }
         private RohGraph _ontologyGraph { get; set; }
         public static DiscoverCacheGlobal _discoverCacheGlobal { get; set; }
 
@@ -52,9 +50,6 @@ namespace API_DISCOVER
         {
             _logger = logger;
             _serviceScopeFactory = serviceScopeFactory;
-            _discoverItemBDService = _serviceScopeFactory.CreateScope().ServiceProvider.GetRequiredService<DiscoverItemBDService>();
-            _processDiscoverStateJobBDService = _serviceScopeFactory.CreateScope().ServiceProvider.GetRequiredService<ProcessDiscoverStateJobBDService>();
-
         }
 
         /// <summary>
@@ -67,21 +62,23 @@ namespace API_DISCOVER
             Guid itemID = JsonConvert.DeserializeObject<Guid>(itemIDstring);
             try
             {
+                DiscoverItemBDService discoverItemBDService = _serviceScopeFactory.CreateScope().ServiceProvider.GetRequiredService<DiscoverItemBDService>();
+                ProcessDiscoverStateJobBDService processDiscoverStateJobBDService = _serviceScopeFactory.CreateScope().ServiceProvider.GetRequiredService<ProcessDiscoverStateJobBDService>();
                 CallCronApiService callCronApiService = _serviceScopeFactory.CreateScope().ServiceProvider.GetRequiredService<CallCronApiService>();
                 CallEtlApiService callEtlApiService = _serviceScopeFactory.CreateScope().ServiceProvider.GetRequiredService<CallEtlApiService>();
                 CallUrisFactoryApiService callUrisFactoryApiService = _serviceScopeFactory.CreateScope().ServiceProvider.GetRequiredService<CallUrisFactoryApiService>();
 
-                DiscoverItem discoverItem = _discoverItemBDService.GetDiscoverItemById(itemID);
+                DiscoverItem discoverItem = discoverItemBDService.GetDiscoverItemById(itemID);
 
                 if (discoverItem != null)
                 {
                     //Aplicamos el proceso de descubrimiento
                     DiscoverResult resultado = Init(discoverItem, callEtlApiService, callUrisFactoryApiService);
                     Process(discoverItem, resultado,
-                        _discoverItemBDService,
+                        discoverItemBDService,
                         callCronApiService,
                         callUrisFactoryApiService,
-                        _processDiscoverStateJobBDService
+                        processDiscoverStateJobBDService
                         );
                 }
             }
