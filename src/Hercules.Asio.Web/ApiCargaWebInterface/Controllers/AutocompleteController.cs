dbbc49d356bd252cf949e1a3b0268dcbf71530fd
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-05-19 13:27:24 +0200

    Añadido buscador

---
+++ src/Hercules.Asio.Web/ApiCargaWebInterface/Controllers/AutocompleteController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using VDS.RDF;
+using VDS.RDF.Query;
+using VDS.RDF.Writing;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+using ApiCargaWebInterface.Models.Services;
+using System.Net;
+using System.IO;
+
+namespace ApiCargaWebInterface.Controllers
+{
+    public class AutocompleteController : Controller
+    {
+        readonly ConfigUrlService _ConfigUrlService;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="configUrlService"></param>
+        public AutocompleteController(ConfigUrlService configUrlService)
+        {
+            _ConfigUrlService = configUrlService;
+        }
+
+        public IActionResult Index(string q)
+        {
+            // Obtiene la URL del servicio al cual hay que hacer la petición.
+            Uri url = new Uri(_ConfigUrlService.GetGraph());
+            string urlPeticion = $@"{url.Scheme}://{url.Host}/autocomplete?q={q}";
+
+            // Hace la petición.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlPeticion);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Transforma el response en string.
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string responseString = reader.ReadToEnd();
+
+            // Devuelve el resultado en Json.
+            List<KeyValuePair<string, string>> resultado = JsonConvert.DeserializeObject<List<KeyValuePair<string, string>>>(responseString);
+            return Json(resultado);
+        }
+
+        public IActionResult GetUrlSearch()
+        {
+            // Obtiene la URL del servicio al cual hay que hacer la petición.
+            Uri url = new Uri(_ConfigUrlService.GetGraph());
+            return Json($@"{url.Scheme}://{url.Host}/Search");
+        }
+    }
+}
--- src/Hercules.Asio.Web/ApiCargaWebInterface/wwwroot/js/site.js
+++ src/Hercules.Asio.Web/ApiCargaWebInterface/wwwroot/js/site.js
@@ -2,3 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+
+    //peticion ajax a 
+    ///autocomplete/GetUrlSearch
+    //
+    $('#formautocompletelds').attr('action', 'xx')
+
+    $.ajax({
+        url: "/autocomplete/GetUrlSearch",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('#formautocompletelds').attr('action', data);
+        }
+    })
+
+    $("#autocompletelds").autocomplete({
+        minLength: 3,
+        source: function (request, response) {
+            $.ajax({
+                url: "/autocomplete",
+                type: "GET",
+                dataType: "json",
+                data: {
+                    q: request.term
+                },
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return {
+                            label: item.Key,
+                            url: item.Value
+                        }
+                    }))
+                }
+            })
+        },
+        select: function (event, ui) {
+            document.location.href = ui.item.url;
+        }
+    })
+});
+
+function replaceAll(string, search, replace) {
+    return string.split(search).join(replace);
+}
