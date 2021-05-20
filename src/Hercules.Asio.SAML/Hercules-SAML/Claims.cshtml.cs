8dfc521a87441b95db14a363ba10057535b47746
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-05-20 11:52:20 +0200

    Creación SAML

---
+++ src/Hercules.Asio.SAML/Hercules-SAML/Claims.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace Hercules_SAML
+{
+    [Authorize]
+    public class ClaimsModel : PageModel
+    {
+        private readonly ILogger<ClaimsModel> _logger;
+
+        public ClaimsModel(ILogger<ClaimsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
