2c5929a2f03b15b17e30be6773c658f5cebea237
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-05-25 12:51:30 +0200

    Update HomeController.cs

--- src/Hercules.Asio.SAML/Hercules-SAML/Controllers/HomeController.cs
+++ src/Hercules.Asio.SAML/Hercules-SAML/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Hercules_SAML.Controllers
             return View();
         }
 
+        public IActionResult Claims()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
