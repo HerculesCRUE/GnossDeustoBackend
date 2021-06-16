a201011978a07563c11be8ac313e00ce008eb09f
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-06-16 11:20:24 +0200

    Update SonarCloud

--- src/Hercules.Asio.Web/ApiCargaWebInterface/Models/Services/VirtualPathProvider/ApiFileProvider.cs
+++ src/Hercules.Asio.Web/ApiCargaWebInterface/Models/Services/VirtualPathProvider/ApiFileProvider.cs
@@ -13,15 +13,10 @@ namespace ApiCargaWebInterface.Models.Services.VirtualPathProvider
 {
     public class ApiFileProvider : IFileProvider
     {
-        private CallApiVirtualPath _callVirtualPath;
+        readonly private CallApiVirtualPath _callVirtualPath;
         public ApiFileProvider(CallApiVirtualPath callVirtualPath)
         {
-            //Stopwatch sw = new Stopwatch(); // Creación del Stopwatch.
-            //sw.Start(); // Iniciar la medición.
-            _callVirtualPath = callVirtualPath;
-            
-            //sw.Stop();
-            //Log.Information($"ApiFileProvider : {sw.Elapsed.ToString("hh\\:mm\\:ss\\.fff")}\n");
+            _callVirtualPath = callVirtualPath;            
         }
 
         public IDirectoryContents GetDirectoryContents(string subpath)
@@ -31,22 +26,14 @@ namespace ApiCargaWebInterface.Models.Services.VirtualPathProvider
 
         public IFileInfo GetFileInfo(string subpath)
         {
-            //Stopwatch sw = new Stopwatch(); // Creación del Stopwatch.
-            //sw.Start(); // Iniciar la medición.
             var result = new ApiFileInfo(_callVirtualPath, subpath);
-            //sw.Stop();
-            //Log.Information($"ApiFileProvider -> GetFileInfo {subpath} : {sw.Elapsed.ToString("hh\\:mm\\:ss\\.fff")}\n");
             return result.Exists ? result as IFileInfo : new NotFoundFileInfo(subpath);
         }
 
         public IChangeToken Watch(string filter)
         {
-            //Stopwatch sw = new Stopwatch(); // Creación del Stopwatch.
-            //sw.Start(); // Iniciar la medición.
             var apiChangeToken = new ApiChangeToken(_callVirtualPath, filter);
-            //Log.Information($"ApiFileProvider -> Watch filter: {filter} : {sw.Elapsed.ToString("hh\\:mm\\:ss\\.fff")}\n");
             return apiChangeToken;
-
         }
     }
 }
