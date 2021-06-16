bc4adb6491c9ec50bb3f42a195ffa4820926eb7c
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-06-16 13:35:48 +0200

    Create ViewBagActionFilter.cs

---
+++ src/Hercules.Asio.Web/ApiCargaWebInterface/ActionFilter/ViewBagActionFilter.cs
@@ -0,0 +1,30 @@
+﻿using ApiCargaWebInterface.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hercules.Asio.Web.ActionFilter
+{
+    public class ViewBagActionFilter : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            string urlLogin = context.HttpContext.RequestServices.GetRequiredService<ConfigUrlService>().GetUrlSAMLLogin();
+            if (!string.IsNullOrEmpty(urlLogin))
+            {
+                // for Razor Views
+                if (context.Controller is Controller)
+                {
+                    var controller = context.Controller as Controller;
+                    controller.ViewBag.SAMLActive = true;
+                }
+            }
+            base.OnResultExecuting(context);
+        }
+    }
+}
