81a6574674b73c4b500676e438a6157c07566a02
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-06-21 11:18:23 +0200

    Update SonarCloud

--- src/Hercules.Asio.Api.Discover/API_DISCOVER/Models/Services/CallTokenService.cs
+++ src/Hercules.Asio.Api.Discover/API_DISCOVER/Models/Services/CallTokenService.cs
@@ -111,7 +111,7 @@ namespace API_DISCOVER.Models.Services
             }
             catch (HttpRequestException)
             {
-                if (!string.IsNullOrEmpty(response.Content.ReadAsStringAsync().Result))
+                if (response != null && !string.IsNullOrEmpty(response.Content.ReadAsStringAsync().Result))
                 {
                     throw new HttpRequestException(response.Content.ReadAsStringAsync().Result);
                 }
