eca662bbcac7e03edfebe57c8f91663e18937a24
alvaropalacios-gnoss <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-07-23 14:26:11 +0200

    Migration

---
+++ src/Hercules.Asio.Api.Carga/API_CARGA/Migrations/20210722074702_UpdateRepositoryConfig_ApplyDiscover.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API_CARGA.Migrations
+{
+    public partial class UpdateRepositoryConfig_ApplyDiscover : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ApplyDiscover",
+                table: "RepositoryConfig",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplyDiscover",
+                table: "RepositoryConfig");
+        }
+    }
+}
