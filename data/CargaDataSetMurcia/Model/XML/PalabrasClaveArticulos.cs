836c2f5d0197c271290653e4a37fc81dab8da979
Adrián <60385160+alvaropalacios-gnoss@users.noreply.github.com>
2021-08-02 12:41:15 +0200

    Actualizada carga

---
+++ data/CargaDataSetMurcia/Model/XML/PalabrasClaveArticulos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargaDataSetMurcia.Model.XML
+{
+    public class PalabrasClaveArticulos
+    {
+        public string PC_ARTI_CODIGO { get; set; }
+        public string PC_PALABRA { get; set; }
+    }
+}
